bb10fecb4186446aa57ef86b85309582194e862a
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-02 20:04:53 +0200

    Интерфейс IFocus
    Добавлен интерфейс для фокусировки геометрических объектов

--- Test/Test/Interfaces.h
+++ Test/Test/Interfaces.h
@@ -5,4 +5,23 @@ struct IMove //интерфейс передвижения
 	virtual void ChangePosition(double dx, double dy) abstract;
 	virtual void SetPosition(double dx, double dy) abstract;
 	virtual void PreviousPosition() abstract;
+	virtual ~IMove() = default;
+};
+
+class IFocus {
+public:
+	IFocus() = delete;
+	IFocus(bool focus) : focus(focus) {}
+	virtual bool getFocus() {
+		return this->focus;
+	}
+	virtual void switchFocus() {
+		this->focus = !this->focus;
+	}
+	virtual void setFocus(bool focus) {
+		this->focus = focus;
+	}
+	virtual ~IFocus() = default;
+private:
+	bool focus;
 };
