3433039c8055765f7933eaf76231ac5a9202b997
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-02 19:45:44 +0200

    Update Interfaces.h

--- Test/Test/Interfaces.h
+++ Test/Test/Interfaces.h
@@ -1,6 +1,6 @@
 #pragma once
 
-class IMove //интерфейс передвижения
+struct IMove //интерфейс передвижения
 {
 	virtual void ChangePosition(double dx, double dy) abstract;
 	virtual void SetPosition(double dx, double dy) abstract;
