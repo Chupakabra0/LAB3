05f97d7700179cd06510f175669a7f067325ced5
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-02 19:36:37 +0200

    Некоторые исправления
    1) замена return -1 на throw
    2) return 0 --> return EXIT_SUCCESS
    3) рефактор

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -1,19 +1,18 @@
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
+int main(void) {
+
+	if (!glfwInit()) throw "Lib isn`t working!";
+
 	GLFWwindow* window;
 
 	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	if (!window) {
 		glfwTerminate();
-		return -1;
+		throw "Window isn`t working!";
 	}
 
 	/* Make the window's context current */
