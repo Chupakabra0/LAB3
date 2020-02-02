5af92dbd4bb1887ff8b6c172ef9d33a17fde1f6b
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-02 20:32:18 +0200

    Добавлен IColor
    Добавлена конвертация в строку множества цветов и интерфейс, где оно используется

--- Test/Test/Interfaces.h
+++ Test/Test/Interfaces.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 enum Color {
 	BLACK,
@@ -18,6 +19,28 @@ enum Color {
 	YELLOW,
 	WHITE
 };
+
+std::string to_string(Color color) {
+	switch (color) {
+	case BLACK: return static_cast<std::string>("Black");
+	case BLUE: return static_cast<std::string>("Blue");
+	case GREEN: return static_cast<std::string>("Green");
+	case CYAN: return static_cast<std::string>("Cyan");
+	case RED: return static_cast<std::string>("Red");
+	case MAGENTA: return static_cast<std::string>("Magenta");
+	case BROWN: return static_cast<std::string>("Brown");
+	case LIGHTGRAY: return static_cast<std::string>("Light gray");
+	case DARKGRAY: return static_cast<std::string>("Dark gray");
+	case LIGHTBLUE: return static_cast<std::string>("Light blue");
+	case LIGHTGREEN: return static_cast<std::string>("Light green");
+	case LIGHTCYAN: return static_cast<std::string>("Light cyan");
+	case LIGHTRED: return static_cast<std::string>("Light red");
+	case LIGHTMAGENTA: return static_cast<std::string>("Light magenta");
+	case YELLOW: return static_cast<std::string>("Yellow");
+	case WHITE: return static_cast<std::string>("White");
+	default: return static_cast<std::string>("Unknown");
+	}
+}
 
 struct IMove //интерфейс передвижения
 {
@@ -41,7 +64,22 @@ public:
 		this->focus = focus;
 	}
 	virtual ~IFocus() = default;
-private:
+protected:
 	bool focus;
 };
 
+
+class IColor {
+public:
+	IColor() = delete;
+	IColor(Color color) : color(color) {}
+	virtual Color getColor() {
+		return this->color;
+	}
+	virtual void setColor(Color color) {
+		this->color = color;
+	}
+	virtual ~IColor() = default;
+protected:
+	Color color;
+};
