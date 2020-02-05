394b1d70414d227dfcf3545d9087ec6c56af715b
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-05 23:16:45 +0200

    Интерфейс масштаба

--- Test/Test/Interfaces.h
+++ Test/Test/Interfaces.h
@@ -2,8 +2,7 @@
 #include <GLFW/glfw3.h>
 #include "XY.h"
 
-__interface IMove //интерфейс передвижения
-{
+__interface IMove { //интерфейс передвижения
 	virtual void ChangePosition(float dx, float dy) abstract;
 	virtual void SetPosition(float x, float y) abstract;
 	virtual void ChangePosition(XY xy) abstract;
@@ -13,14 +12,20 @@ __interface IMove //интерфейс передвижения
 	virtual void PreviousPosition() abstract;
 };
 
-__interface IRotate //интерфейс поворота
-{
+__interface IRotate { //интерфейс поворота 
 	virtual void ChangeAngle(float angle) abstract;
 	virtual void SetAngle(float angle) abstract;
 	virtual void RememberAngle(float angle) abstract;
 	virtual void PreviousAngle() abstract;
 };
 
-__interface  IDraw { //интерфейс рисования
+__interface IDraw { //интерфейс рисования
 	virtual void Draw() abstract;
+};
+
+__interface IScale {
+	virtual void ChangeScale(float percent) abstract;
+	virtual void SetScale(float percent) abstract;
+	virtual void RememberScale(float percent) abstract;
+	virtual void PreviousScale() abstract;
 };
