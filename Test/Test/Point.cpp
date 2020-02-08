7ee0a5092af60bc713d5e48909a13c1217736bab
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-08 18:39:37 +0200

    Убрал поворот для точки

--- Test/Test/Point.cpp
+++ Test/Test/Point.cpp
@@ -10,7 +10,7 @@ Point::Point(float x): Point(x, x) {}
 Point::Point(float x, float y): Point(x, y, 0.f) {}
 Point::Point(float x, float y, RGB color) : Point(XY(x, y), color){  }
 
-Point::Point(XY xy, RGB color) : dot(xy), color(color), angle(0.f), scale(1.f) {
+Point::Point(XY xy, RGB color) : dot(xy), color(color), scale(1.f) {
 	this->Remember();
 }
 
@@ -25,7 +25,6 @@ void Point::Previous() {
 		this->history.pop_back();
 		const auto size = this->history.size() - 1;
 		if (this->GetDot() != this->history[size].GetDot()) this->SetDotWithoutR(this->history[size].GetDot());
-		if (this->GetAngle() != this->history[size].GetAngle()) this->SetAngleWithoutR(this->history[size].GetAngle());
 		if (this->GetScale() != this->history[size].GetScale()) this->SetScaleWithoutR(this->history[size].GetScale());
 		if (this->GetColor() != this->history[size].GetColor()) this->SetColorWithoutR(this->history[size].GetColor());
 	}
@@ -33,8 +32,6 @@ void Point::Previous() {
 }
 
 Scale Point::GetScale() const { return this->scale; }
-
-Angle Point::GetAngle() const { return this->angle; }
 
 Dot Point::GetDot() const { return this->dot; }
 
@@ -44,11 +41,6 @@ RGB Point::GetColor() const {
 
 void Point::SetScale(const Scale& scale) {
 	this->scale.SetScale(scale.GetScale());
-	this->Remember();
-}
-
-void Point::SetAngle(const Angle& angle) {
-	this->angle.SetAngle(angle.GetAngle());
 	this->Remember();
 }
 
@@ -63,13 +55,11 @@ void Point::SetColor(const RGB& color) {
 }
 
 void Point::Remember() {
-	this->history.emplace_back(this->GetDot(), this->GetScale(), this->GetAngle(), this->GetColor());
+	this->history.emplace_back(this->GetDot(), this->GetScale(), NULL, this->GetColor());
 }
 
 Point::~Point() = default;
 void Point::SetScaleWithoutR(const Scale& scale) { this->scale.SetScale(scale.GetScale()); }
-
-void Point::SetAngleWithoutR(const Angle& angle) { this->angle.SetAngle(angle.GetAngle()); }
 
 void Point::SetDotWithoutR(const Dot& dot) { this->dot.SetPosition(dot.GetPosition()); }
 
--- Test/Test/Point.h
+++ Test/Test/Point.h
@@ -26,21 +26,18 @@ public:
 	RGB GetColor() const;
 	//---------------------------------------------------------------------
 	void SetScale(const Scale& scale);
-	void SetAngle(const Angle& angle);
 	void SetDot(const Dot& dot);
 	void SetColor(const RGB& color);
 	//---------------------------------------------------------------------
 	virtual ~Point();
 protected:
 	void SetScaleWithoutR(const Scale& scale);
-	void SetAngleWithoutR(const Angle& angle);
 	void SetDotWithoutR(const Dot& dot);
 	void SetColorWithoutR(const RGB& color);
 	//---------------------------------------------------------------------
 	void Remember();
 	//---------------------------------------------------------------------
 	Scale scale;
-	Angle angle;
 	Dot dot;
 	RGB color;
 	std::vector<History> history;
