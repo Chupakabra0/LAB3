ee826215996832f46ac01b58cc062e68e6a6e330
Сафиюлин Александр <rootalfa4@gmail.com>
2020-02-22 13:24:35 +0200

    Движение по диагонали вернулось

--- Test/Test/Source.cpp
+++ Test/Test/Source.cpp
@@ -19,61 +19,44 @@ struct Const {
 	constexpr static float SCALE_PLUS = 1.f;
 	constexpr static float SCALE_MINUS = -1.f;
 
-	static void Key(std::vector<IShape*>& shapes, const Event& event, float& time) {
+	static void Key(std::vector<IShape*>& shapes, Event& event, float& time) {
 		if (time >= Const::TIME) {
 			if (!shapes.empty() && event.type == Event::KeyPressed) {
-				switch (event.key.code) {
-				case Keyboard::Key::Up: {
-					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(0.f, -MOVE));
-					break;
-				}
-				case Keyboard::Key::Down: {
-					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(0.f, MOVE));
-					break;
-				}
-				case Keyboard::Key::Left: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Left)) {
 					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(-MOVE, 0.f));
-					break;
 				}
-				case Keyboard::Key::Right: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Right)) {
 					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(MOVE, 0.f));
-					break;
 				}
-				case Keyboard::Key::E: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Up)) {
+					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(0.f, -MOVE));
+				}
+				if (Keyboard::isKeyPressed(Keyboard::Key::Down)) {
+					ShapeDealer::Move(dynamic_cast<IMove*>(shapes[shapes.size() - 1]), XY(0.f, MOVE));
+				}
+				if (Keyboard::isKeyPressed(Keyboard::Key::E)) {
 					ShapeDealer::Rotate(dynamic_cast<IRotate*>(shapes[shapes.size() - 1]), Angle(-ANGLE));
-					break;
 				}
-				case Keyboard::Key::Q: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Q)) {
 					ShapeDealer::Rotate(dynamic_cast<IRotate*>(shapes[shapes.size() - 1]), Angle(ANGLE));
-					break;
 				}
-				case Keyboard::Key::W: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::W)) {
 					ShapeDealer::Zoom(dynamic_cast<IScale*>(shapes[shapes.size() - 1]), Scale(SCALE_PLUS));
-					break;
 				}
-				case Keyboard::Key::S: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
 					ShapeDealer::Zoom(dynamic_cast<IScale*>(shapes[shapes.size() - 1]), Scale(SCALE_MINUS));
-					break;
 				}
-				case Keyboard::Key::Z: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::Z)) {
 					ShapeDealer::LegacyCondition(dynamic_cast<IShape*>(shapes[shapes.size() - 1]));
-					break;
 				}
-				case Keyboard::Key::X: {
+				if (Keyboard::isKeyPressed(Keyboard::Key::X)) {
 					ShapeDealer::FirstCondition(dynamic_cast<IShape*>(shapes[shapes.size() - 1]));
-					break;
-				}
-				default: break;
 				}
 			}
 			else if (event.type == Event::KeyReleased) {
-				switch (event.key.code) {
-				case Keyboard::Key::Add: {
+				if (event.key.code == Keyboard::Key::Add) {
 					Circle* temp = new Circle(XY(200.f), Color(100, 255, 30, 255), 2.f);
 					shapes.push_back(dynamic_cast<IDraw*>(temp));
-					break;
-				}
-				default: break;
 				}
 			}
 			time = 0.f;
