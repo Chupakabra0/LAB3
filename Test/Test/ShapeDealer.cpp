2d04aa5371a01c431866e7e53875690e93d754db
Сафиюлин Александр <rootalfa4@gmail.com>
2020-03-24 13:30:39 +0200

    Рефакторинг файлов ShapeDealer

--- Test/Test/ShapeDealer.cpp
+++ Test/Test/ShapeDealer.cpp
@@ -1,1 +1,84 @@
 #include "ShapeDealer.h"
+
+void ShapeDealer::Draw(IDraw* draw, sf::RenderWindow& window) {
+	draw->Draw(window);
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::Move(IMove* move, XY xy) {
+	move->ChangePosition(xy);
+	move->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SetPosition(IMove* move, XY xy) {
+	move->SetPosition(xy);
+	move->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::Rotate(IRotate* rotate, Angle angle) {
+	rotate->ChangeAngle(angle);
+	rotate->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SetAngle(IRotate* rotate, Angle angle) {
+	rotate->SetAngle(angle);
+	rotate->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SetColor(IDraw* object, sf::Color color) {
+	object->SetColor(color);
+	object->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::Zoom(IScale* object, Scale scale) {
+	object->ChangeScale(scale);
+	object->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SetScale(IScale* object, Scale scale) {
+	object->SetScale(scale);
+	object->RememberCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::LegacyCondition(IHistory* shape) {
+	shape->PreviousCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::FirstCondition(IHistory* shape) {
+	shape->FirstCondition();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SwitchFocus(Figure* figure) {
+	figure->setFocus(!figure->getFocus());
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SwitchVisible(Figure* figure) {
+	figure->setVisible(!figure->getVisible());
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::SwitchTrace(Figure* figure) {
+	figure->setTrace(!figure->getTrace());
+	if (figure->getTrace()) figure->setTraceNum(figure->getHistory().size() - 1);
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+Figure* ShapeDealer::MakeCopy(Figure* figure) {
+	return figure->Copy();
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+void ShapeDealer::ObstacleScale(std::vector<Figure*>& shapes, int index) {
+	for (auto i = 0; i < shapes.size(); i++) {
+		if (i != index) {
+			if (CountRadius(shapes[index], shapes[i]) <= shapes[index]->getRadius() + shapes[i]->getRadius()) {
+				shapes[index]->setTouch(true);
+			}
+			else if (shapes[index]->getTouch()) {
+				shapes[index]->setTouch(false);
+			}
+		}
+	}
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+float ShapeDealer::CountRadius(Figure* first, Figure* second) {
+	return sqrtf(powf(second->GetPosition().getX() - first->GetPosition().getX(), 2)
+		+
+		powf(second->GetPosition().getY() - first->GetPosition().getY(), 2));
+}
--- Test/Test/ShapeDealer.h
+++ Test/Test/ShapeDealer.h
@@ -2,77 +2,33 @@
 #include "Interfaces.h"
 #include "Circle.h"
 
-struct ShapeDealer
-{
+struct ShapeDealer {
 	ShapeDealer() = delete;
 	~ShapeDealer() = delete;
-	//---------------------------------------------------------------------
-	static void Draw(IDraw* draw, sf::RenderWindow& window) {
-		draw->Draw(window);
-	}
-	static void Move(IMove* move, XY xy) {
-		move->ChangePosition(xy);
-		move->RememberCondition();
-	}
-	static void SetPosition(IMove* move, XY xy) {
-		move->SetPosition(xy);
-		move->RememberCondition();
-	}
-	static void Rotate(IRotate* rotate, Angle angle) {
-		rotate->ChangeAngle(angle);
-		rotate->RememberCondition();
-	}
-	static void SetAngle(IRotate* rotate, Angle angle) {
-		rotate->SetAngle(angle);
-		rotate->RememberCondition();
-	}
-	static void SetColor(IDraw* object, sf::Color color) {
-		object->SetColor(color);
-		object->RememberCondition();
-	}
-	static void Zoom(IScale* object, Scale scale) {
-		object->ChangeScale(scale);
-		object->RememberCondition();
-	}
-	static void SetScale(IScale* object, Scale scale) {
-		object->SetScale(scale);
-		object->RememberCondition();
-	}
-	static void LegacyCondition(IHistory* shape) {
-		shape->PreviousCondition();
-	}
-	static void FirstCondition(IHistory* shape) {
-		shape->FirstCondition();
-	}
-	static void SwitchFocus(Figure* figure) {
-		figure->setFocus(!figure->getFocus());
-	}
-	static void SwitchVisible(Figure* figure) {
-		figure->setVisible(!figure->getVisible());
-	}
-	static void SwitchTrace(Figure* figure) {
-		figure->setTrace(!figure->getTrace());
-		if (figure->getTrace()) figure->setTraceNum(figure->getHistory().size() - 1);
-	}	
-	static Figure* MakeCopy(Figure* figure) {
-		return figure->Copy();
-	}
-	static void ObstacleScale(std::vector<Figure*>& shapes, int index) {
-		for (auto i = 0; i < shapes.size(); i++) {
-			if (i != index) {
-				if (CountRadius(shapes[index], shapes[i]) <= shapes[index]->getRadius() + shapes[i]->getRadius()) {
-					shapes[index]->setTouch(true);
-				}
-				else if (shapes[index]->getTouch()) {
-					shapes[index]->setTouch(false);
-				}
-			}
-		}
-	}
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void Draw(IDraw* draw, sf::RenderWindow& window);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void Move(IMove* move, XY xy);
+	static void SetPosition(IMove* move, XY xy);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void Rotate(IRotate* rotate, Angle angle);
+	static void SetAngle(IRotate* rotate, Angle angle);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void SetColor(IDraw* object, sf::Color color);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void Zoom(IScale* object, Scale scale);
+	static void SetScale(IScale* object, Scale scale);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void LegacyCondition(IHistory* shape);
+	static void FirstCondition(IHistory* shape);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void SwitchFocus(Figure* figure);
+	static void SwitchVisible(Figure* figure);
+	static void SwitchTrace(Figure* figure);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static Figure* MakeCopy(Figure* figure);
+	//-----------------------------------------------------------------------------------------------------------------------------------------------
+	static void ObstacleScale(std::vector<Figure*>& shapes, int index);
 private:
-	static float CountRadius(Figure* first, Figure* second) {
-		return sqrtf(powf(second->GetPosition().getX() - first->GetPosition().getX(), 2)
-						+
-						powf(second->GetPosition().getY() - first->GetPosition().getY(), 2));
-	}
+	static float CountRadius(Figure* first, Figure* second);
 };
